0a27395f95ffc8dab2b1d7046e425b8a3e16c0e7
izzettunc <izzet.tunc1997@gmail.com>
2026-03-24 07:23:37 +0100

    Simplified version provider for cli and added unit tests

--- src/main/java/com/lyr/cli/util/VersionProvider.java
+++ src/main/java/com/lyr/cli/util/VersionProvider.java
@@ -1,16 +1,25 @@
 package com.lyr.cli.util;
 
+import org.apache.commons.lang3.StringUtils;
 import picocli.CommandLine;
 
 public class VersionProvider implements CommandLine.IVersionProvider {
 
+    public static Package getPackage() {
+        return VersionProvider.class.getPackage();
+    }
+
     public static String getVersionFromManifest() {
-        final Package pkg = VersionProvider.class.getPackage();
-        return (pkg != null) ? pkg.getImplementationVersion() : "unknown";
+        final Package pkg = getPackage();
+        if (pkg != null && !StringUtils.isBlank(pkg.getImplementationVersion())) {
+            return pkg.getImplementationVersion();
+        } else {
+            return "unknown";
+        }
     }
 
     @Override
-    public String[] getVersion() throws Exception {
+    public String[] getVersion() {
         return new String[] {getVersionFromManifest()};
     }
 }
