3ce612f0aaa4e44e1ae1ba0c4b9f4446b269c5df
izzettunc <izzet.tunc1997@gmail.com>
2026-04-25 08:51:29 +0200

    Added configuration field to execution as described on schema

--- lyr-report/src/main/java/com/lyr/report/model/Execution.java
+++ lyr-report/src/main/java/com/lyr/report/model/Execution.java
@@ -1,15 +1,18 @@
 package com.lyr.report.model;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import lombok.Builder;
 
 @Builder
-public record Execution(String name, String code, ImmutableList<Finding> findings) {
+public record Execution(
+        String name, String code, ImmutableMap<String, String> configuration, ImmutableList<Finding> findings) {
 
     public static Execution copyOf(final Execution execution) {
         return Execution.builder()
                 .name(execution.name())
                 .code(execution.code())
+                .configuration(ImmutableMap.copyOf(execution.configuration))
                 .findings(execution.findings.stream().map(Finding::copyOf).collect(ImmutableList.toImmutableList()))
                 .build();
     }
