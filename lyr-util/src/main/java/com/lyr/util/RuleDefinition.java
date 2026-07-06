f57324165f712f256e9f7fc99b621dd5e04ae6b5
izzettunc <izzet.tunc1997@gmail.com>
2026-07-06 20:28:16 +0200

    Changed category to cloud-provider in rule code format

--- lyr-util/src/main/java/com/lyr/util/RuleDefinition.java
+++ lyr-util/src/main/java/com/lyr/util/RuleDefinition.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 public enum RuleDefinition {
     // Syntax:
     // ruleName: scan.<service>.<resource>.<check>
-    // ruleCode: <category>-<service>-<increment-id>
+    // ruleCode: <cloud-provider>-<service>-<increment-id>
     // Rule code is three letter for each section
 
     SCAN_CLOUDWATCH_LOG_GROUP_WITHOUT_RETENTION_POLICY(
@@ -20,6 +20,10 @@ public enum RuleDefinition {
     SCAN_DYNAMODB_TABLE_IDLE("scan.dynamodb.table.idle", "WST-DDB-001"),
 
     SCAN_LAMBDA_FUNCTION_WITH_UNBOUNDED_CONCURRENCY("scan.lambda.function.withUnboundedConcurrency", "WST-LMD-001");
+            "scan.cloudwatch.logGroup.withoutRetentionPolicy", "AWS-CLW-001"),
+    SCAN_GLUE_SESSION_ACTIVE_WITH_LONG_IDLE_TIMEOUT("scan.glue.session.activeWithLongIdleTimeout", "AWS-GLU-001"),
+    SCAN_DYNAMODB_TABLE_IDLE("scan.dynamodb.table.idle", "AWS-DDB-001"),
+    SCAN_LAMBDA_FUNCTION_WITH_UNBOUNDED_CONCURRENCY("scan.lambda.function.withUnboundedConcurrency", "AWS-LMD-001"),
 
     private static final Map<String, RuleDefinition> NAME_TO_DEFINITION_MAP = new HashMap<>();
     private static final Map<String, RuleDefinition> CODE_TO_DEFINITION_MAP = new HashMap<>();
