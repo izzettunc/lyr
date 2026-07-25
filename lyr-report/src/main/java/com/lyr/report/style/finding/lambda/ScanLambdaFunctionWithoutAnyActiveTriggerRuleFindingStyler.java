b34235fed525c058ecdb501f859f9573fdc45d88
izzettunc <izzet.tunc1997@gmail.com>
2026-07-25 14:15:39 +0200

    Fixed scan lambda function without any active trigger rule finding style

--- lyr-report/src/main/java/com/lyr/report/style/finding/lambda/ScanLambdaFunctionWithoutAnyActiveTriggerRuleFindingStyler.java
+++ lyr-report/src/main/java/com/lyr/report/style/finding/lambda/ScanLambdaFunctionWithoutAnyActiveTriggerRuleFindingStyler.java
@@ -19,7 +19,8 @@ public class ScanLambdaFunctionWithoutAnyActiveTriggerRuleFindingStyler implemen
         final StringBuilder reportBuilder = new StringBuilder();
         for (final Finding finding : findings) {
             final var findingReport = String.format(
-                    "Lambda function '%s' doesn't have any active event source that can trigger it.", finding.identifier());
+                    "Lambda function '%s' doesn't have any active event source that can trigger it.",
+                    finding.identifier());
             final var styledFindingReport = PlainTextReportStyler.styleFindingReport(findingReport, Sentiment.NEGATIVE);
             reportBuilder.append(PlainTextReportStyler.toNewLine(styledFindingReport));
         }
